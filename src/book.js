9d7f384838b3329e7732ddb545957d920f345baa
Taylor Phillips <taylorscottphillips@gmail.com>
2021-03-11 10:29:50 -0600

    Complete createTitle and buildMainCharacter methods

--- src/book.js
+++ src/book.js
@@ -1,11 +1,21 @@
-function createTitle() {
+function createTitle(title) {
+  return "The " + title;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return { name: name, age: age, pronouns: pronouns };
+}
+
+function saveReview(review, reviews) {
+  reviews.push(review);
   
+  return [...new Set(reviews)];
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
